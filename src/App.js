5fda7d51918918ee5c1d957c8152a30459c19403
TheJmqn <jman4461@gmail.com>
2023-12-27 23:39:24 -0500

    ESPN API load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const load = async () => {
+    const api = "https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2023/types/2/teams/12/statistics";
+    try {
+      await axios.get(api).then(res => {console.log(res); setData(res.data);});
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={load}>Load</button>
+      <br></br>
+      {data == null ? "Not Loaded." : data.splits.categories[1].stats[5].displayValue}
     </div>
   );
 }
